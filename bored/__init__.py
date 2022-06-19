5fd79ddf9bf79c74fed4a762753c494c5edb50f2
Damego <danyabatueff@gmail.com>
2022-06-19 18:34:59 +0500

    refactor: rename folder to bored_api

--- bored/__init__.py
+++
@@ -1,3 +0,0 @@
-from .bored_api import BoredActivity, BoredClient, ActivityType
-
-__version__ = "1.0.0"
---
+++ bored_api/__init__.py
@@ -0,0 +1,3 @@
+from .bored_api import BoredActivity, BoredClient, ActivityType
+
+__version__ = "1.0.0"

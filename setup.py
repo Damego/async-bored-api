efb2361b716917424283b916c59f02e30c1c15d9
Damego <danyabatueff@gmail.com>
2022-06-19 21:08:14 +0500

    pypi: add python3.10

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+		"Programming Language :: Python :: 3.10",
         "Topic :: Internet",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Software Development :: Libraries",
